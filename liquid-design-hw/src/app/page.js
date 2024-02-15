2ca698b5370b42565d0c87f0a2aa048e79a6a1bd
Anthony Lopez <anthonylopez@Anthonys-MacBook-Air.local>
2024-02-15 10:45:12 -0600

    card index

--- liquid-design-hw/src/app/page.js
+++ liquid-design-hw/src/app/page.js
@@ -1,9 +1,10 @@
+import Card from "@/components/Card";
 
 
 export default function Home() {
   return (
     <main className="bg">
-      hola
+      <Card time="30 mins" price1='clasic' price2='Élite'/>
     </main>
   );
 }
---
+++ liquid-design-hw/src/components/Button/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({text, price, onClick, children, customClass, href=''}) => {
+  return (
+    <div>Button</div>
+  )
+}
+
+export default Button
