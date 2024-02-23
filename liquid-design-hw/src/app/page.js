2cceb23c26fcd92a817f9646d0a33d6e8c1fce61
Anthony Lopez <anthonylopez@Anthonys-MacBook-Air.local>
2024-02-23 09:45:03 -0600

    removed card

--- liquid-design-hw/src/app/page.js
+++ liquid-design-hw/src/app/page.js
@@ -1,12 +1,10 @@
-import Card from "@/components/Card";
 import Footer from "@/components/Footer";
 
 
 export default function Home() {
   return (
-    <main className="">
-      <Card time="30 mins" price1='clasic' price2='Élite'/>
+    <>
       <Footer />
-    </main>
+    </>
   );
 }
